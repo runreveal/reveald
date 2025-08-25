115bb9db5f5ad4022e77afd07d4609a95f1e26a9
Alan Braithwaite <alan@runreveal.com>
2025-08-25 09:15:14 -0400

    remove filesystem for now

--- internal/destinations/objstore/config.go
+++ internal/destinations/objstore/config.go
@@ -1,9 +1,6 @@
 package objstore
 
 import (
-	"os"
-	"slices"
-
 	"github.com/runreveal/lib/loader"
 )
 
@@ -15,12 +12,6 @@ func init() {
 		return &S3Config{}
 	})
 
-	// only load if not in stage/prod
-	if slices.Contains([]string{"development", "testing", ""}, os.Getenv("RUNREVEAL_ENV")) {
-		loader.Register("fs", func() loader.Builder[BlobLike] {
-			return &FilesystemConfig{}
-		})
-	}
 }
 
 func (r R2Config) Configure() (BlobLike, error) {
@@ -29,8 +20,4 @@ func (r R2Config) Configure() (BlobLike, error) {
 
 func (s S3Config) Configure() (BlobLike, error) {
 	return NewS3(s)
-}
-
-func (f FilesystemConfig) Configure() (BlobLike, error) {
-	return NewFilesystem(f)
 }
